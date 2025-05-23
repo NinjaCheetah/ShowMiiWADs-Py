0d8b7d24bdcbcb656b6b9af913ca2c6afb0a25e5
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2025-05-22 22:03:15 -0400

    Fix Windows workflow run

--- Build.ps1
+++ Build.ps1
@@ -9,6 +9,7 @@ $buildShowMiiWADsPy = {
     Write-Host "Building ShowMiiWADs-Py..."
     # python build_translations.py
     cd rust_modules && python -m maturin develop --release
+    cd ..
     python -m nuitka --show-progress --assume-yes-for-downloads ShowMiiWADs-Py.py
 }
 
